89becb685e088726cbfdae8faa3443896d976389
Özgür Barış Ayhan <64589883+ayhanozgurbaris@users.noreply.github.com>
2022-12-02 11:57:34 +0300

    userLoginPage

--- ws/ui/src/UserLoginPage.js
+++ ws/ui/src/UserLoginPage.js
@@ -2,23 +2,55 @@ import React, { Component } from 'react';
 
 export class UserLoginPage extends Component {
 
+
+    state = {
+        username:null,
+        password:null,
+    }
+
+
+    onChangeUsername = (event) => {
+        this.setState({
+           username:event.target.value
+        })
+       };
+
+    onChangePassword = event =>{
+        this.setState({
+            password:event.target.value
+        })
+     }
+
+
+     onClickSignUp = event =>{
+        event.preventDefault();
+
+        const body = {
+            username:this.state.username ,
+            password: this.state.password
+         };
+         
+     }
+
+
+
+
     render() {
         return (
             <form>
-                 <h1>Sign Up</h1>
+                 <h1>Login</h1>
             <div>
                 <label>Username</label>
+                <input onChange={this.onChangeUsername}></input>
             </div>
 
             
             <div>
                 <label>Password</label>
+                <input type="password" onChange={this.onChangePassword}></input>
             </div>
 
-            
-            <div>
-                <label>Username</label>
-            </div>
+
 
             <button>Login</button>
 
