b6bd7078e8d6bfa98ef84f8b159d09e36026db94
Özgür Barış Ayhan <64589883+ayhanozgurbaris@users.noreply.github.com>
2022-12-02 16:18:14 +0300

    defaultForDb

--- ws/src/main/java/com/socialize/ws/WsApplication.java
+++ ws/src/main/java/com/socialize/ws/WsApplication.java
@@ -1,14 +1,36 @@
 package com.socialize.ws;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.socialize.ws.user.User;
+import com.socialize.ws.user.UserService;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner createInitialUsers(UserService userService) {
+		return new CommandLineRunner() {
+
+			@Override
+			public void run(String... args) throws Exception {
+				User user = new User();
+				user.setUsername("defaultUser");
+				user.setDisplayName("defaultDisplay");
+				user.setPassword("defaultPassword");
+				userService.save(user);
+			}
+			
+		};
+		
 	}
 
 }
