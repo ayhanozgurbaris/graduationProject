b1ab638b19ec484a8f946523f432e15bf7aa3886
Özgür Barış Ayhan <64589883+ayhanozgurbaris@users.noreply.github.com>
2022-12-04 15:52:01 +0300

    userItemList

--- ws/ui/src/UserItemList.js
+++ ws/ui/src/UserItemList.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import defaultPicture from './profile.png'
+import { Link } from 'react-router-dom';
 
 const UserItemList = props =>{
     const { user } = props;
+    const {username,displayName,image}  = user;
 
     let imageSource = defaultPicture;
-    if(user.image){
+    if(image){
         imageSource = user.image;
     }
 
  
         return (
-            <div className='list-group-item list-group-item-action'>
-                <img width={32} alt={`${user.username} profile`} src={imageSource}/>
-                    {user.username}
-            </div>
+            <Link to={`/user/${username}`} className='list-group-item list-group-item-action'>
+                <img width={32} alt={`${username} profile`} src={imageSource}/>
+                {displayName}@{username}
+            </Link>
         );
     
 
